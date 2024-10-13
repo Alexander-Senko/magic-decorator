Alexander-Senko/magic-lookup

Framework: generated a new gem
* Ruby 3.2+
* RSpec with `its`
* RuboCop

lib/magic/lookup/authors.rb
@@ -1,0 +1,27 @@
+# frozen_string_literal: true
+
+Gem::Author ||= Struct.new(
+		:name,
+		:email,
+		:github,
+) do
+	def github_url = github && "https://github.com/#{github}"
+end
+
+module Magic
+	module Lookup
+		AUTHORS = [
+				Gem::Author.new(
+						name:   'Alexander Senko',
+						email:  'Alexander.Senko@gmail.com',
+						github: 'Alexander-Senko',
+				),
+		]
+
+		class << AUTHORS
+			def names      = filter_map &:name
+			def emails     = filter_map &:email
+			def github_url = filter_map(&:github_url).first
+		end
+	end
+end

lib/magic/lookup/version.rb
@@ -1,0 +1,7 @@
+# frozen_string_literal: true
+
+module Magic
+	module Lookup
+		VERSION = '0.1.0'
+	end
+end

spec/spec_helper.rb
@@ -1,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rspec/its'
+
+require 'magic/lookup'
+
+RSpec.configure do |config|
+	# Enable flags like --only-failures and --next-failure
+	config.example_status_persistence_file_path = '.rspec_status'
+
+	# Disable RSpec exposing methods globally on `Module` and `main`
+	config.disable_monkey_patching!
+
+	config.expect_with :rspec do |c|
+		c.syntax = :expect
+	end
+end

Alexander-Senko/magic-decorator

lib/magic/decorator/authors.rb
@@ -1,0 +1,27 @@
+# frozen_string_literal: true
+
+Gem::Author ||= Struct.new(
+		:name,
+		:email,
+		:github,
+) do
+	def github_url = github && "https://github.com/#{github}"
+end
+
+module Magic
+	module Decorator
+		AUTHORS = [
+				Gem::Author.new(
+						name:   'Alexander Senko',
+						email:  'Alexander.Senko@gmail.com',
+						github: 'Alexander-Senko',
+				),
+		]
+
+		class << AUTHORS
+			def names      = filter_map &:name
+			def emails     = filter_map &:email
+			def github_url = filter_map(&:github_url).first
+		end
+	end
+end

lib/magic/decorator/version.rb
@@ -1,0 +1,7 @@
+# frozen_string_literal: true
+
+module Magic
+	module Decorator
+		VERSION = '0.1.0'
+	end
+end

spec/spec_helper.rb
@@ -1,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rspec/its'
+
+require 'magic/decorator'
+
+RSpec.configure do |config|
+	# Enable flags like --only-failures and --next-failure
+	config.example_status_persistence_file_path = '.rspec_status'
+
+	# Disable RSpec exposing methods globally on `Module` and `main`
+	config.disable_monkey_patching!
+
+	config.expect_with :rspec do |c|
+		c.syntax = :expect
+	end
+end